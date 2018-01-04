2d9d87ec8fe9c83b1d49475db6b07accf0dfd838
yitimo <admin@yitimo.com>
2018-01-04 17:39:17 +0800

    add localhost:8080 white list

--- kyouko/connect.go
+++ kyouko/connect.go
@@ -8,7 +8,7 @@ import (
 // ConnectInit 连接守卫
 func ConnectInit(m *martini.ClassicMartini) {
 	m.Use(cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"http://localhost:3001", "https://www.yitimo.com", "https://yitimo.com"},
+		AllowOrigins:     []string{"http://localhost:8080", "http://localhost:3001", "https://www.yitimo.com", "https://yitimo.com"},
 		AllowMethods:     []string{"GET", "POST"},
 		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
