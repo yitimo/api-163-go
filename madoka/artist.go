7613baec4064d71afddd2a1d2a1f82bca56e3f51
yitimo <admin@yitimo.com>
2018-01-17 12:02:20 +0800

    add artist info with hotSong api

--- madoka/artist.go
+++ madoka/artist.go
@@ -17,3 +17,21 @@ func ArtistTop(page, limit int) (string, error) {
 	}
 	return res, nil
 }
+
+// ArtistSong - get songs if artist by id
+func ArtistSong(id string, page, limit int) (string, error) {
+	// 1. transfer page, limit into offset, limit
+	_offset, _limit := formatParams(page, limit)
+	// 2. encode request params
+	preParams := "{\"csrf_token\": \"\"}"
+	params, encSecKey, encErr := EncParams(preParams)
+	if encErr != nil {
+		return "", encErr
+	}
+	// 3. request, resolve, return
+	res, resErr := post("http://music.163.com/weapi/v1/artist/"+id+"?offset="+_offset+"&limit="+_limit, params, encSecKey)
+	if resErr != nil {
+		return "", resErr
+	}
+	return res, nil
+}
--- madoka/common.go
+++ madoka/common.go
@@ -6,7 +6,31 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"math/rand"
+	"time"
 )
+
+var userAgentList = [19]string {
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
+    "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89;GameHelper",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A300 Safari/602.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:46.0) Gecko/20100101 Firefox/46.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:46.0) Gecko/20100101 Firefox/46.0",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+    "Mozilla/5.0 (Windows NT 6.3; Win64, x64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/13.10586",
+    "Mozilla/5.0 (iPad; CPU OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A300 Safari/602.1",
+}
 
 func get(_url string) (string, error) {
 	res, resErr := http.Get(_url)
@@ -19,6 +43,34 @@ func get(_url string) (string, error) {
 	return string(rs), rsErr
 }
 
+func pget(_url string, params map[string]string) (string, error) {
+	// 创建客户端
+	client := &http.Client{}
+	// 设置body
+	form := url.Values{}
+	for key, value := range params {
+		form.Set(key, value)
+	}
+	body := strings.NewReader(form.Encode())
+	// 创建请求
+	request, _ := http.NewRequest("POST", _url, body)
+	//设置头部
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Set("Cookie", "appver=2.0.2")
+	request.Header.Set("Referer", "http://music.163.com")
+	clen := body.Len()
+	request.Header.Set("Content-Length", strconv.Itoa(clen))
+	// 发起请求
+	response, reqErr := client.Do(request)
+	// 错误处理
+	if reqErr != nil {
+		return "", reqErr
+	}
+	defer response.Body.Close()
+	resBody, resErr := ioutil.ReadAll(response.Body)
+	return string(resBody), resErr
+}
+
 func post(_url, params, encSecKey string) (string, error) {
 	client := &http.Client{}
 	form := url.Values{}
@@ -28,7 +80,8 @@ func post(_url, params, encSecKey string) (string, error) {
 	request, _ := http.NewRequest("POST", _url, body)
 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	request.Header.Set("Referer", "http://music.163.com")
-	request.Header.Set("Content-Length", (string)(body.Len()))
+	request.Header.Set("Content-Length", strconv.Itoa(body.Len()))
+	request.Header.Set("User-Agent", fakeAgent())
 	// 发起请求
 	response, reqErr := client.Do(request)
 	// 错误处理
@@ -55,4 +108,9 @@ func post(_url, params, encSecKey string) (string, error) {
 		limit = 0
 	}
 	return strconv.Itoa((page - 1) * limit), strconv.Itoa(limit)
+}
+
+func fakeAgent() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return userAgentList[r.Intn(19)]
 }
